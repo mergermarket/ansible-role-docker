53f53ba29165a4aa89327b5f640a286260ab3e99
Marcio Goda <marciogoda@gmail.com>
2021-04-16 10:35:08 +0100

    Update docker_housekeeping.sh

--- files/docker_housekeeping.sh
+++ files/docker_housekeeping.sh
@@ -5,7 +5,7 @@ set -e
 # Check to see if the user-data has run 
 if [ -f /var/log/user-data-has-run ] ; then
     docker ps -aq | xargs docker container stop || echo "Dont fail"
-    docker system prune -af --filter 'label!=type=platform' --filter 'until=72h'
+    docker system prune -f --filter 'label!=type=platform' --filter 'until=72h'
     docker volume prune -f
     docker network prune -f
     docker system df
